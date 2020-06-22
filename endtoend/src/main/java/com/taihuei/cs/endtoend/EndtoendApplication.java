74be96adeacdb1be9a93916d5eb8e3719512eb41
taihuei0114 <63032621+taihuei0114@users.noreply.github.com>
2020-06-22 20:33:01 +0800

    first commit

---
+++ endtoend/src/main/java/com/taihuei/cs/endtoend/EndtoendApplication.java
@@ -0,0 +1,13 @@
+package com.taihuei.cs.endtoend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtoendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndtoendApplication.class, args);
+	}
+
+}
